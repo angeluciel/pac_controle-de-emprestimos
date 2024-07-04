422c9c258f222e5b9ae1ed718f0aed653d514190
Luciel <jpmizidoro@gmail.com>
2024-07-04 16:17:57 -0300

    mudança
    alterei de api para backend, e de front para frontend. além disso mudei a porta da API para 3009.

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ app.use('/items', itemRoutes);
 app.use('/usuarios', usuarioRoutes);
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3009;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
